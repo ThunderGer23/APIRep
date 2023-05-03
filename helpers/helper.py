6c5f28278c9b4439c2ee3258759f5d4695c6a2a8
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2023-05-03 00:13:16 -0600

    Prototipo de graficos

--- helpers/helper.py
+++ helpers/helper.py
@@ -5,6 +5,9 @@ from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
+#Para la grafica
+import matplotlib.pyplot as plt
+
 #Para las img
 from reportlab.lib.utils import ImageReader
 
@@ -13,6 +16,15 @@ def createText(c,w:float,h:float,den:int,x:int,y:int,tamLet:int,text:str):
     title.setFont("FUTURAM",tamLet)
     title.textLine(text)
     return c.drawText(title)
+
+def createImg(porPlag:list,section:str):
+    docs=['D1','D2','D3','D4','D5','D6','D7','D8','D9','D10','D11','D12','D13','D14','D15','D16',]
+    fig,ax=plt.subplots()
+    #Etiqueta eje y
+    ax.set_title('% Plagio')
+    #Creamos la grafica de barras utilizando 'docs' como X y 'porPlag' como y
+    plt.bar(docs,porPlag)
+    plt.savefig(section)
 
 
 def createFile(nameFile: str,):
@@ -56,12 +68,18 @@ def createFile(nameFile: str,):
 
     #Primer Subtitulo lado Izquierdo
     createText(c,w,h,14,-15,-170,14,'Introducción:')
+    porPlag=[25,50,100,60,12,14,28,26,36,80,70,45,38,19,25,50]
+    createImg(porPlag,'Introduccion')
+    img_Intro=ImageReader("Introduccion.png")
+    #Ancho y alto de la imagen
+    img_wIn,img_hIn=img_Intro.getSize()
+    c.drawImage(img_Intro,14,h-334,164,164)
 
     #Segundo Subtitulo lado Izquierdo
-    createText(c,w,h,14,-15,-300,14,'Marco Teórico:')
+    createText(c,w,h,14,-15,-340,14,'Marco Teórico:')
 
     #Titulo lado Derecho
-    createText(c,w,h,2,50,-140,18,'Reporte de Bibliografía')
+    createText(c,w,h,2,50,-120,18,'Reporte de Bibliografía')
 
     #Segunda Línea Horizontal
     c.line(x,350,w-50,350)
