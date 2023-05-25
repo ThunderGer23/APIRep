84da8ec3520bbf907be1c5d83e965da7e07d50f9
ThunderGer <Jaames11@hotmail.com>
2023-05-25 00:29:30 -0600

    Refactor a dibImagen

--- helpers/helper.py
+++ helpers/helper.py
@@ -11,17 +11,17 @@ W,H=A4
 def dibImagen(name:str, c, x:int, y:int, px:int, py:int):
     img=ImageReader(name)
     img_w,img_h=img.getSize()
+    # if y!=0 and px==0 and py==0:
+    #     return c.drawImage(img,x,y)
+    # elif x==0 and y==0 and px==0 and py==0:
+    #     return c.drawImage(img,W-img_h,H-img_h)
+    # elif px == 0 and py==0:
+    #     return c.drawImage(img,x,H-img_h)
+    # else:
+    #     return c.drawImage(img,x,H+y,px,py)
     
-    if y!=0 and px==0 and py==0:
-        return c.drawImage(img,x,y)
-    elif x==0 and y==0 and px==0 and py==0:
-        return c.drawImage(img,W-img_h,H-img_h)
-    elif px == 0 and py==0:
-        return c.drawImage(img,x,H-img_h)
-    else:
-        return c.drawImage(img,x,H+y,px,py)
-    
-    # return (c.drawImage(img, x, y) if y != 0 else (c.drawImage(img, W - img_h, H - img_h) if x == 0 else c.drawImage(img, x, H - img_h))) if px == 0 and py == 0 else c.drawImage(img, x, H + y, px, py)
+    return (((c.drawImage(img,x,H-img_h),c.drawImage(img,W-img_h,H-img_h))[x == 0 and y == 0],c.drawImage(img,x,y))[y != 0],c.drawImage(img,x,H+y,px,py))[px == 0 and py == 0]
+
 
 
 def createText(c,den:int,x:int,y:int,tamLet:int,text:str):
