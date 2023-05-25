17e81343c333fb4e2b236c294b3db392b15da8e9
ThunderGer <jaames11@hotmail.com>
2023-05-24 18:22:48 -0600

    Refactor de Simulación

--- helpers/process.py
+++ helpers/process.py
@@ -1,6 +1,11 @@
 from time import sleep as sl
+from notigram import ping
 
 def messagealert():
-    while True:
-        print("Enviando mensaje")
-        sl(4)
+    time = 1800
+    ping('daa39d53-6283-47a1-b945-b7ee6528dde0', 'Conteo de tiempo para la emulación')
+    while time > 0:
+        ping('daa39d53-6283-47a1-b945-b7ee6528dde0', f'Restan {time} seg')
+        # print("Enviando mensaje")
+        time-=1
+        sl(1)
