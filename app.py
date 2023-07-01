db8fa535de09e298bf1882eec69b55de3c615602
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2023-06-30 23:38:37 -0600

    Ahi esta la linea de código >:|

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routes.rep import rep
+from notigram import ping
 
 app = FastAPI()
 ping('daa39d53-6283-47a1-b945-b7ee6528dde0', f'APIRep Reportandose, Kawamonga')
